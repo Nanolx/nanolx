87b89e4ae9b3a2428337136cdfe552f4df371070
Christopher Roy Bratušek <nano@jpberlin.de>
2026-09-08 20:57:57 +0200

    splitt off more packages

--- plasmoids/KdeControlStation/contents/ui/js/helpers.js
+++
@@ -1,9 +0,0 @@
-function listProperty(item) {
-    for (var p in item)
-    {
-        if( typeof item[p] != "function" )
-            if(p != "objectName")
-                console.log(p + ":" + item[p]);
-    }
-
-}
--- plasmoids/luisbocanegra.advanced_separator/contents/ui/code/enum.js
+++
@@ -1,6 +0,0 @@
-const ColorSourceType = {
-  Custom: 0,
-  System: 1,
-  Random: 2,
-  List: 3,
-};
